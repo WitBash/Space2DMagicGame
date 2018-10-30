e33782a28816355dbafac4032eb480cc8a16bf8f
WitBash <yr-83p@mail.ru>
2018-10-30 18:50:56 +0300

    add buttons play and close

--- core/src/com/witbash/screen/PlayScreen.java
+++ core/src/com/witbash/screen/PlayScreen.java
@@ -1,21 +1,16 @@
 package com.witbash.screen;
 
-import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.math.Vector2;
-import com.witbash.Space2DMagic;
 import com.witbash.base.Base2DScreen;
 import com.witbash.math.Rect;
 import com.witbash.sprite.Background;
-import com.witbash.sprite.ButtonClose;
-import com.witbash.sprite.ButtonPlay;
 import com.witbash.sprite.Star;
 
-public class MenuScreen extends Base2DScreen {
+public class PlayScreen extends Base2DScreen {
 
     private static final int STAR_COUNT = 256;
 
@@ -25,14 +20,6 @@ public class MenuScreen extends Base2DScreen {
     private TextureAtlas textureAtlas;
     private Star[] stars;
 
-    private ButtonPlay buttonPlay;
-    private ButtonClose buttonClose;
-
-    private Game playScreen;
-
-    public MenuScreen(Game playScreen) {
-        this.playScreen = playScreen;
-    }
 
     @Override
     public void show() {
@@ -44,8 +31,6 @@ public class MenuScreen extends Base2DScreen {
         for (int i = 0; i < stars.length; i++) {
             stars[i] = new Star(textureAtlas);
         }
-        buttonPlay = new ButtonPlay(textureAtlas);
-        buttonClose = new ButtonClose(textureAtlas);
     }
 
     @Override
@@ -69,12 +54,6 @@ public class MenuScreen extends Base2DScreen {
         for (int i = 0; i < stars.length; i++) {
             stars[i].draw(batch);
         }
-        buttonPlay.draw(batch);
-        buttonPlay.setRight(0.23f);
-        buttonPlay.setTop(0.38f);
-        buttonClose.draw(batch);
-        buttonClose.setRight(0.38f);
-        buttonClose.setTop(0.5f);
         batch.end();
     }
 
@@ -84,8 +63,6 @@ public class MenuScreen extends Base2DScreen {
         for (int i = 0; i < stars.length; i++) {
             stars[i].resize(worldBounds);
         }
-        buttonPlay.resize(worldBounds);
-        buttonClose.resize(worldBounds);
     }
 
     @Override
@@ -93,25 +70,5 @@ public class MenuScreen extends Base2DScreen {
         bgTexture.dispose();
         textureAtlas.dispose();
         super.dispose();
-    }
-
-    @Override
-    public boolean touchDown(Vector2 touch, int pointer) {
-        if (buttonClose.isMe(touch)) buttonClose.setScale(0.5f);
-        if (buttonPlay.isMe(touch)) buttonPlay.setScale(0.5f);
-        return false;
-    }
-
-    @Override
-    public boolean touchUp(Vector2 touch, int pointer) {
-        if (buttonClose.isMe(touch)) {
-            buttonClose.setScale(1f);
-            Gdx.app.exit();
-        }
-        if (buttonPlay.isMe(touch)) {
-            buttonPlay.setScale(1f);
-            playScreen.setScreen(new PlayScreen());
-        }
-        return false;
     }
 }
--- core/src/com/witbash/sprite/ButtonPlay.java
+++ core/src/com/witbash/sprite/ButtonPlay.java
@@ -3,11 +3,10 @@ package com.witbash.sprite;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.witbash.base.Sprite;
 
-public class ButtonClose extends Sprite {
+public class ButtonPlay extends Sprite {
 
-    public ButtonClose(TextureAtlas atlas) {
-        super(atlas.findRegion("Close"));
+    public ButtonPlay(TextureAtlas atlas) {
+        super(atlas.findRegion("play"));
         setHeightProportion(0.15f);
     }
 }
-
